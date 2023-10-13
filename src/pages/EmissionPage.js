5f0ca27d7e58ecada0c647dd5c15da822feadc19
Aashish Panthi <aashishpanthi11@gmail.com>
2023-10-13 14:53:49 +0545

    Update EmissionPage.js

--- src/pages/EmissionPage.js
+++ src/pages/EmissionPage.js
@@ -6,7 +6,7 @@ function EmissionPage() {
     <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-3 mt-5 h-screen">
       <h1 className="text-3xl font-bold text-gray-900">
         CO<sub>2</sub> Emission Analysis and Prediction of Nepal from 1960 to
-        2022
+        2025
       </h1>
 
       <div className="mt-16 flex flex-col justify-center">
