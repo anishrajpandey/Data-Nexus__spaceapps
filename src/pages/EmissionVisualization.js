1e451a9ab79a140f0a617bc3839391022edec978
Aashish Panthi <aashishpanthi11@gmail.com>
2023-10-13 15:56:50 +0545

    Update EmissionVisualization.js

--- src/pages/EmissionVisualization.js
+++ src/pages/EmissionVisualization.js
@@ -10,7 +10,7 @@ function EmissionPage() {
     <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-3 mt-5">
       <h1 className="text-3xl font-bold text-gray-900">
         CO<sub>2</sub> Emission Visualization of different countries from year
-        1960 to 2022
+        1960 to 2020
       </h1>
 
       <div className="mt-4 flex flex-col items-start">
